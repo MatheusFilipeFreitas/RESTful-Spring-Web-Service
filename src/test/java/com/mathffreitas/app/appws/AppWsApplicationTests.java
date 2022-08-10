277ad0b7f94e1ff277b69302fda322a2b2bc2053
Matheus Freitas <mathffreitas@hotmail.com>
2022-08-10 19:24:32 -0300

    Project Structure

---
+++ src/test/java/com/mathffreitas/app/appws/AppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathffreitas.app.appws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
