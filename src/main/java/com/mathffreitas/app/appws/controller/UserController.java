4169f44312aa9616fe1b6f347035c064890a0acb
Matheus Freitas <mathffreitas@hotmail.com>
2022-08-11 10:47:08 -0300

    Creating UserDetail entity

--- src/main/java/com/mathffreitas/app/appws/controller/UserController.java
+++ src/main/java/com/mathffreitas/app/appws/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.mathffreitas.app.appws.controller;
 
+import com.mathffreitas.app.appws.model.UserDetail;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,7 +13,7 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser() {
+    public String createUser(@RequestBody UserDetail userDetails) {
         return "create user was called";
     }
 
