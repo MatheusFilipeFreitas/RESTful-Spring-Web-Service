a32bf5465aeb41816a6ec2e012c8aaf518ccc8ee
Matheus Freitas <mathffreitas@hotmail.com>
2022-08-15 03:46:00 -0300

    Customize Exception messages

--- src/main/java/com/mathffreitas/app/appws/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/mathffreitas/app/appws/exceptions/AppExceptionsHandler.java
@@ -1,2 +1,26 @@
-package com.mathffreitas.app.appws.exceptions;public class AppExceptionsHandler {
+package com.mathffreitas.app.appws.exceptions;
+
+import com.mathffreitas.app.appws.model.response.ErrorMessage;
+import com.mathffreitas.app.appws.model.response.ErrorMessages;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class AppExceptionsHandler {
+
+    @ExceptionHandler( value = {UserServiceException.class} )
+    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
+
+        ErrorMessage errorMessageTemplate = new ErrorMessage(new Date(), ex.getMessage());
+
+        return new ResponseEntity<>(errorMessageTemplate, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
 }
